1272b267ce8edb562c81d052396293d94afa9582
John Lee <john.cmlee@gmail.com>
2020-12-22 19:58:44 -0500

    Updated todolist

--- Lessons/custom_reduce.rb
+++ Lessons/custom_reduce.rb
@@ -1,16 +1,17 @@
-def reduce(array, initial = 0)
-  sum = initial
-  counter = 0
+def reduce(array, initial_value = nil)
+  accumulator = initial_value ? initial_value + array.first : array.first
+  counter = 1
   while counter < array.size
-    element = array[counter]
-    sum = yield(sum, element)
+    accumulator = yield(accumulator, array[counter])
     counter += 1
   end
-  sum
+  accumulator
 end
 
 array = [1, 2, 3, 4, 5]
 
 p reduce(array) { |acc, num| acc + num }                    # => 15
 p reduce(array, 10) { |acc, num| acc + num }                # => 25
-reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+# reduce(array) { |acc, num| acc + num if num.odd? }        # => NoMethodError: undefined method `+' for nil:NilClass
+p reduce(['a', 'b', 'c']) { |acc, value| acc += value }     # => 'abc'
+p reduce([[1, 2], ['a', 'b']]) { |acc, value| acc + value } # => [1, 2, 'a', 'b']
--- Lessons/launch_todolist.rb
+++ Lessons/launch_todolist.rb
@@ -111,4 +111,34 @@ class TodoList
   def to_a
     @todos.clone
   end
+
+  def each
+    @todos.each do |todo|
+      yield(todo)
+    end
+    self
+  end
+
+  def select
+    selection = TodoList.new(title)
+    each do |todo|
+      selection.add(todo) if yield(todo)
+    end
+    selection
+  end
 end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+
+list = TodoList.new("Today's Todos")
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+todo1.done!
+
+results = list.select { |todo| todo.done? }    # you need to implement this method
+
+puts results
